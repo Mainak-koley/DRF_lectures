f815c9005713453bb27364d8f62283d01eb8d57a
Mainak-koley <mkoley120@gmail.com>
2025-10-07 19:35:37 +0530

    add html type form to send data

--- app1/urls.py
+++ app1/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path , include
-from .views import add_product , get_all_product, update_product
+from .views import add_product 
+from .views import ProductViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'product/' , ProductViewSet)
+
 urlpatterns = [
+     path('', include(router.urls)),
     path('add_product/', add_product , name="add_product"),
-    path('get_product_list/', get_all_product , name="list_product"),
-    path('update_product_list/', update_product , name="update_product"),   
+    # path('get_product_list/', get_all_product , name="list_product"),
+    # path('update_product_list/', update_product , name="update_product"),   
 ]
--- app1/views.py
+++ app1/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import ProductModel
-# Create your views here.
+from rest_framework import viewsets
 
 @api_view(['POST'])
 def add_product(request):
@@ -17,28 +17,34 @@ def add_product(request):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['GET'])
-def get_all_product(request):
-    products = ProductModel.objects.all()
-    product = ProductSerializer(products, many= True)
-    if product:
-        return Response(product.data)
-    else:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+# @api_view(['GET'])
+# def get_all_product(request):
+#     products = ProductModel.objects.all()
+#     product = ProductSerializer(products, many= True)
+#     if product:
+#         return Response(product.data)
+#     else:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['PATCH'])
-def update_product(request):
-    id = request.data.get('id')
+# @api_view(['PATCH'])
+# def update_product(request):
+#     id = request.data.get('id')
     
-    try:
-        product = ProductModel.objects.get(id=id)
-    except:
-        return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+#     try:
+#         product = ProductModel.objects.get(id=id)
+#     except:
+#         return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
     
-    serializer_class = ProductSerializer(product, data=request.data, partial=True)
-    if serializer_class.is_valid():
-        serializer_class.save()
-        return Response(serializer_class.data)
-    else:
-        return Response(status=status.HTTP_404_NOT_FOUND)
- 
+#     serializer_class = ProductSerializer(product, data=request.data, partial=True)
+#     if serializer_class.is_valid():
+#         serializer_class.save()
+#         return Response(serializer_class.data)
+#     else:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = ProductModel.objects.all()
